dbd6185695db1ec7067d4dd225dace00d799cca1
Nhung Nguyen <nhungnth8.neu@gmail.com>
2023-07-07 22:38:44 +0700

    add Topic 04 Run on browser

--- selenium/webdriver/Topic_03_Xpath_Css.java
+++ selenium/webdriver/Topic_03_Xpath_Css.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_03_Xpath_Part_1 {
+public class Topic_03_Xpath_Css {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
