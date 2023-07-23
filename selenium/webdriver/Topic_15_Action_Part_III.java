e9e9ab67b44c3c2be3beae75214723abb8926da1
Leng Keng PC <Leng Keng PC@DESKTOP-3H1RF2P>
2023-07-23 17:15:53 +0700

    update topic 15 action part 3

--- selenium/webdriver/Topic_15_Action_Part_III.java
+++ selenium/webdriver/Topic_15_Action_Part_III.java
@@ -1,16 +1,28 @@
 package webdriver;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -22,6 +34,7 @@ public class Topic_15_Action_Part_III {
 	JavascriptExecutor jsExecutor;
 	String projectPath = System.getProperty("user.dir");
 	String osName = System.getProperty("os.name");
+	String drapDropHelperPath = projectPath + "\\drapAndDrop\\drag_and_drop_helper.js";
 
 		
 	@BeforeClass
@@ -59,7 +72,7 @@ public class Topic_15_Action_Part_III {
 
 
 
-	@Test
+	//@Test
 	public void TC_02_Right_Click() {
 		driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
 		
@@ -82,9 +95,78 @@ public class Topic_15_Action_Part_III {
 
 	}
 
-	@Test
-	public void TC_03_() {
+	//@Test
+	public void TC_03_Drap_And_Drop_HTML4() {
+		driver.get("https://automationfc.github.io/kendo-drag-drop/");
+		
+		WebElement smallCircle = driver.findElement(By.cssSelector("div#draggable"));
+		
+		WebElement bigCircle = driver.findElement(By.cssSelector("div#droptarget"));
+		
+		action.dragAndDrop(smallCircle, bigCircle).perform();
+		sleepInSecond(3);
+		
+		// Verify text
+		Assert.assertEquals(bigCircle.getText(), "You did great!");
+		
+		// Verify background color (sử dụng thư viện color của selenium)
+		String bigCircleRGB = bigCircle.getCssValue("background-color");
+		System.out.println(bigCircleRGB);
+		
+		String bigCircleHEXA = Color.fromString(bigCircleRGB).asHex();
+		System.out.println(bigCircleHEXA);
+		
+		Assert.assertEquals(bigCircleHEXA.toUpperCase(),"#03A9F4");
 	}   
+	
+	//@Test
+	public void TC_04_Drap_And_Drop_HTML5() throws IOException {
+		String jsHelper = getContentFile(drapDropHelperPath);
+		
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		
+		String sourceCss = "div#column-a";
+		String targetCss = "div#column-b";	
+		
+		jsHelper = jsHelper + "$(\"" + sourceCss + "\").simulateDragDrop({ dropTarget: \"" + targetCss + "\"});";
+		
+		// A to B
+		jsExecutor.executeScript(jsHelper);
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='B']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='A']")).isDisplayed());
+
+		// B to A
+		jsExecutor.executeScript(jsHelper);
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='A']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='B']")).isDisplayed());
+
+	 
+	}   
+	
+	@Test
+	public void TC_05_Drap_And_Drop_HTML5() throws IOException, AWTException {
+		driver.get("https://automationfc.github.io/drag-drop-html5/");
+		
+		// A to B
+		dragAndDropHTML5ByXpath("//div[@id='column-a']","//div[@id='column-b']");
+		sleepInSecond(3);
+		
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='B']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='A']")).isDisplayed());
+		
+		// B to A
+		dragAndDropHTML5ByXpath("//div[@id='column-b']","//div[@id='column-a']");
+		sleepInSecond(3);
+
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-a']/header[text()='A']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='column-b']/header[text()='B']")).isDisplayed());
+
+	}
+
 
 	
 	public void sleepInSecond(long timeInSecond) {
@@ -96,6 +178,65 @@ public class Topic_15_Action_Part_III {
 		}
 	}
 	
+	public void dragAndDropHTML5ByXpath(String sourceLocator, String targetLocator) throws AWTException {
+
+		WebElement source = driver.findElement(By.xpath(sourceLocator));
+		WebElement target = driver.findElement(By.xpath(targetLocator));
+
+		// Setup robot
+		Robot robot = new Robot();
+		robot.setAutoDelay(500);
+
+		// Get size of elements
+		Dimension sourceSize = source.getSize();
+		Dimension targetSize = target.getSize();
+
+		// Get center distance
+		int xCentreSource = sourceSize.width / 2;
+		int yCentreSource = sourceSize.height / 2;
+		int xCentreTarget = targetSize.width / 2;
+		int yCentreTarget = targetSize.height / 2;
+
+		Point sourceLocation = source.getLocation();
+		Point targetLocation = target.getLocation();
+
+		// Make Mouse coordinate center of element
+		sourceLocation.x += 20 + xCentreSource;
+		sourceLocation.y += 110 + yCentreSource;
+		targetLocation.x += 20 + xCentreTarget;
+		targetLocation.y += 110 + yCentreTarget;
+
+		// Move mouse to drag from location
+		robot.mouseMove(sourceLocation.x, sourceLocation.y);
+
+		// Click and drag
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mouseMove(((sourceLocation.x - targetLocation.x) / 2) + targetLocation.x, ((sourceLocation.y - targetLocation.y) / 2) + targetLocation.y);
+
+		// Move to final position
+		robot.mouseMove(targetLocation.x, targetLocation.y);
+
+		// Drop
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+	
+	public String getContentFile(String filePath) throws IOException {
+		Charset cs = Charset.forName("UTF-8");
+		FileInputStream stream = new FileInputStream(filePath);
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(stream, cs));
+			StringBuilder builder = new StringBuilder();
+			char[] buffer = new char[8192];
+			int read;
+			while ((read = reader.read(buffer, 0, buffer.length)) > 0) {
+				builder.append(buffer, 0, read);
+			}
+			return builder.toString();
+		} finally {
+			stream.close();
+		}
+	}
 	@AfterClass
 	public void afterClass() {
 		//driver.quit();
