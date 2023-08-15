59f769482916ab6146c2429593a60016cc2ae3d4
Nhung Nguyen <nhungnth8.neu@gmail.com>
2023-08-15 23:31:25 +0700

    topic testNG part III

---
+++ selenium/facebook/webmobile/Post_01.java
@@ -0,0 +1,23 @@
+package facebook.webmobile;
+
+import org.testng.annotations.Test;
+
+
+
+public class Post_01 {
+
+	@Test
+	public void TC_01() {
+		
+	}
+	
+	@Test
+	public void TC_02() {
+		
+	}
+	
+	@Test
+	public void TC_03() {
+		
+	}
+}
